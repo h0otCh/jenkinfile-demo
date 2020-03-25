59a731d4a1fa441d82cb45b9f57c0fbd2fb6fd40
Chris <purzelification@gmail.com>
2020-03-25 18:29:10 +0100

    adds controller

---
+++ src/main/java/com/example/demo/Hello.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+public class Hello {
+	private String answer;
+
+	public Hello() {
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+}
---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping(value = "hello/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Hello> greeting(@PathVariable String name) {
+		Hello hello = new Hello();
+		hello.setAnswer(name);
+		return new ResponseEntity<>(hello, HttpStatus.OK);
+	}
+}
